7126b418f9cf1715eefd45fa71a90314d877b1f0
Shreya-Monu <shreyajakkula.1612@gmail.com>
2026-03-05 14:58:24 +0530

    initial commit

---
+++ part-4/calc.js
@@ -0,0 +1,14 @@
+// export function substract (a,b){
+//     return a-b
+// }
+// export function add(a,b){
+//     return a+b
+// }
+// export {add,substract}
+//if only single function then we use default
+export default function add(a,b){
+    return a+b
+}
+export function sub(a,b){
+    return a-b
+}
---
+++ part-4/page33.js
@@ -0,0 +1,5 @@
+import "./calc.js"
+const result = add(4,6)
+console.log(result)
+// const result2 = substract(10,3)
+// console.log(result2)
